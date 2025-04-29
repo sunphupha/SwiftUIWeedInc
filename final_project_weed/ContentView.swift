236eeaece0252351d4672496a6bdd96fc80e765c
Phupha Kongkangwannatee <sunphupha12345@gmail.com>
2025-04-29 23:37:09 +0700

    Initial Commit

---
+++ final_project_weed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final_project_weed
+//
+//  Created by Sun Phupha on 29/4/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ final_project_weed/final_project_weedApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_project_weedApp.swift
+//  final_project_weed
+//
+//  Created by Sun Phupha on 29/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct final_project_weedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
